00b5d355f9cccf747553c2a181e9110753c98cff
tcpip147 <tcpip147@gmail.com>
2024-05-01 08:51:11 +0900

    Fixing the problem of getting go to an unrelated token when clicking a control

--- src/main/java/com/tcpip147/querybook/GotoHandler.java
+++ src/main/java/com/tcpip147/querybook/GotoHandler.java
@@ -1,12 +1,21 @@
 package com.tcpip147.querybook;
 
 import com.intellij.codeInsight.navigation.actions.GotoDeclarationHandler;
+import com.intellij.codeInsight.template.emmet.tokens.StringLiteralToken;
+import com.intellij.ide.highlighter.JavaFileType;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.FileTypeManager;
+import com.intellij.openapi.fileTypes.FileTypes;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiJavaToken;
 import com.intellij.psi.PsiManager;
+import com.intellij.psi.impl.light.JavaIdentifier;
+import com.intellij.psi.impl.source.tree.ElementType;
+import com.intellij.psi.impl.source.tree.java.PsiJavaTokenImpl;
 import com.intellij.psi.search.FileTypeIndex;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.tcpip147.querybook.util.DocumentUtils;
@@ -19,19 +28,23 @@ public class GotoHandler implements GotoDeclarationHandler {
 
     @Override
     public PsiElement @Nullable [] getGotoDeclarationTargets(@Nullable PsiElement psiElement, int offset, Editor editor) {
-        String find = psiElement.getText().replaceAll("\"", "");
-        Project project = psiElement.getProject();
-        Collection<VirtualFile> files = FileTypeIndex.getFiles(QueryBookFileType.INSTANCE, GlobalSearchScope.projectScope(project));
-        for (VirtualFile file : files) {
-            PsiFile psiFile = PsiManager.getInstance(project).findFile(file);
-            PsiElement xmlDocument = DocumentUtils.findFirstChildByName(psiFile, "PsiElement(XML_DOCUMENT)");
-            if (xmlDocument != null) {
-                PsiElement queryMap = DocumentUtils.findFirstChildByName(xmlDocument, "XmlTag:queryMap");
-                if (queryMap != null) {
-                    List<PsiElement> queryList = DocumentUtils.findChildrenByName(queryMap, "XmlTag:query");
-                    for (PsiElement query : queryList) {
-                        if (find.equals(DocumentUtils.getAttributeValue(query, "id"))) {
-                            return new PsiElement[]{query};
+        if (editor.getVirtualFile().getFileType() == JavaFileType.INSTANCE) {
+            if (psiElement instanceof PsiJavaTokenImpl && ((PsiJavaTokenImpl) psiElement).getTokenType() == ElementType.STRING_LITERAL) {
+                String find = psiElement.getText().replaceAll("\"", "");
+                Project project = psiElement.getProject();
+                Collection<VirtualFile> files = FileTypeIndex.getFiles(QueryBookFileType.INSTANCE, GlobalSearchScope.projectScope(project));
+                for (VirtualFile file : files) {
+                    PsiFile psiFile = PsiManager.getInstance(project).findFile(file);
+                    PsiElement xmlDocument = DocumentUtils.findFirstChildByName(psiFile, "PsiElement(XML_DOCUMENT)");
+                    if (xmlDocument != null) {
+                        PsiElement queryMap = DocumentUtils.findFirstChildByName(xmlDocument, "XmlTag:queryMap");
+                        if (queryMap != null) {
+                            List<PsiElement> queryList = DocumentUtils.findChildrenByName(queryMap, "XmlTag:query");
+                            for (PsiElement query : queryList) {
+                                if (find.equals(DocumentUtils.getAttributeValue(query, "id"))) {
+                                    return new PsiElement[]{query};
+                                }
+                            }
                         }
                     }
                 }
--- src/main/java/com/tcpip147/querybook/QueryBookCompletionContributor.java
+++ src/main/java/com/tcpip147/querybook/QueryBookCompletionContributor.java
@@ -8,6 +8,8 @@ import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
+import com.intellij.psi.impl.source.tree.ElementType;
+import com.intellij.psi.impl.source.tree.java.PsiJavaTokenImpl;
 import com.intellij.psi.search.FileTypeIndex;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.util.ProcessingContext;
@@ -22,23 +24,26 @@ public class QueryBookCompletionContributor extends CompletionContributor {
     public QueryBookCompletionContributor() {
         extend(CompletionType.BASIC, PlatformPatterns.psiElement(), new CompletionProvider<>() {
             public void addCompletions(@NotNull CompletionParameters parameters, @NotNull ProcessingContext context, @NotNull CompletionResultSet resultSet) {
-                if ("PsiJavaToken:STRING_LITERAL".equals(parameters.getPosition().toString())) {
-                    String literal = parameters.getPosition().getText();
-                    literal = literal.substring(1, literal.length() - 1);
-                    literal = literal.replaceAll("IntellijIdeaRulezzz ", "");
-                    Project project = parameters.getPosition().getProject();
-                    Collection<VirtualFile> files = FileTypeIndex.getFiles(QueryBookFileType.INSTANCE, GlobalSearchScope.projectScope(project));
-                    for (VirtualFile file : files) {
-                        PsiFile psiFile = PsiManager.getInstance(project).findFile(file);
-                        PsiElement xmlDocument = DocumentUtils.findFirstChildByName(psiFile, "PsiElement(XML_DOCUMENT)");
-                        if (xmlDocument != null) {
-                            PsiElement queryMap = DocumentUtils.findFirstChildByName(xmlDocument, "XmlTag:queryMap");
-                            if (queryMap != null) {
-                                List<PsiElement> queryList = DocumentUtils.findChildrenByName(queryMap, "XmlTag:query");
-                                for (PsiElement query : queryList) {
-                                    String id = DocumentUtils.getAttributeValue(query, "id");
-                                    if (id.length() >= literal.length() && id.substring(0, literal.length()).equals(literal)) {
-                                        resultSet.addElement(LookupElementBuilder.create(id));
+                PsiElement psiElement = parameters.getPosition();
+                if (psiElement != null) {
+                    if (psiElement instanceof PsiJavaTokenImpl && ((PsiJavaTokenImpl) psiElement).getTokenType() == ElementType.STRING_LITERAL) {
+                        String literal = psiElement.getText();
+                        literal = literal.substring(1, literal.length() - 1);
+                        literal = literal.replaceAll("IntellijIdeaRulezzz ", "");
+                        Project project = psiElement.getProject();
+                        Collection<VirtualFile> files = FileTypeIndex.getFiles(QueryBookFileType.INSTANCE, GlobalSearchScope.projectScope(project));
+                        for (VirtualFile file : files) {
+                            PsiFile psiFile = PsiManager.getInstance(project).findFile(file);
+                            PsiElement xmlDocument = DocumentUtils.findFirstChildByName(psiFile, "PsiElement(XML_DOCUMENT)");
+                            if (xmlDocument != null) {
+                                PsiElement queryMap = DocumentUtils.findFirstChildByName(xmlDocument, "XmlTag:queryMap");
+                                if (queryMap != null) {
+                                    List<PsiElement> queryList = DocumentUtils.findChildrenByName(queryMap, "XmlTag:query");
+                                    for (PsiElement query : queryList) {
+                                        String id = DocumentUtils.getAttributeValue(query, "id");
+                                        if (id.length() >= literal.length() && id.substring(0, literal.length()).equals(literal)) {
+                                            resultSet.addElement(LookupElementBuilder.create(id));
+                                        }
                                     }
                                 }
                             }
